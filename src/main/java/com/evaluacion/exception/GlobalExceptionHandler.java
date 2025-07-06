c57be677a2e464f84a663ee79fbc3bbcada7830d
Jorge Saez Castillo <jorge.saez.c1@gmail.com>
2025-07-06 16:15:30 -0400

    chore: add uuid exception

--- src/main/java/com/evaluacion/exception/GlobalExceptionHandler.java
+++ src/main/java/com/evaluacion/exception/GlobalExceptionHandler.java
@@ -4,9 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,6 +18,14 @@ public class GlobalExceptionHandler {
         Map<String, String> error = new HashMap<>();
         error.put("mensaje", ex.getMessage());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> handleUUIDFormatError(MethodArgumentTypeMismatchException ex) {
+        if (ex.getRequiredType() == UUID.class) {
+            return ResponseEntity.badRequest().body(Map.of("mensaje", "El ID no tiene un formato UUID válido"));
+        }
+        throw ex;
     }
 
 }
